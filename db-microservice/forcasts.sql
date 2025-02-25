23a2a596b9bcb2fbafecc11663399a422ae46280
Harrison <htflynn0@gmail.com>
2025-02-25 13:56:11 +0000

    Add users table to db

--- db-microservice/forcasts.sql
+++ db-microservice/forcasts.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    user_id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- should be created automatically
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE IF NOT EXISTS forecasts (
     id SERIAL PRIMARY KEY,
     location VARCHAR(100) NOT NULL,
